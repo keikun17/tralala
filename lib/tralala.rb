a2acfc7ba8bb8f5eeeffd559f3fe6fc93660121c
keikun17 <keikun17@gmail.com>
2013-02-28 21:20:35 +0800

    gemify

--- lib/tralala.rb
+++ lib/tralala.rb
@@ -1,5 +1,34 @@
 require "tralala/version"
 
 module Tralala
-  # Your code goes here...
+
+  extend ActiveSupport::Concern
+
+  def dingdingdong
+    puts "You touch my tralala : #{caller[1]}"
+  end
+
+
+  def save
+    dingdingdong
+    super
+  end
+
+  def save!
+    dingdingdong
+    super
+  end
+
+  def update
+    dingdingdong
+    super
+  end
+
+  def create
+    dingdingdong
+    super
+  end
+
 end
+
+ActiveRecord::Base.send(:include, Tralala)
